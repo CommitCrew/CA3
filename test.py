b342c3c54c8ef13d68522186363ff6518de56110
zuhaumar <113166540+zuhaumar@users.noreply.github.com>
2023-09-19 19:46:56 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 from calculator import app
+from calculator import app
+from flask import jsonify
 
 def test_add():
     with app.test_client() as client:
         response = client.get('/add/10/22')
         assert response.get_data(as_text=True) == '32'
+
+def test_multiply():
+    with app.test_client() as client:
+        response = client.get('/multiply/2/3')
+        assert response.get_data(as_text=True) == "6"
