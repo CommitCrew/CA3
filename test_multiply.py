6cb583f921b3880c864d1c68eac3c4f801f59125
zuhaumar <113166540+zuhaumar@users.noreply.github.com>
2023-09-19 19:50:03 +0500

    Delete test_multiply.py

--- test_multiply.py
+++
@@ -1,6 +0,0 @@
-from calculator import app
-from flask import jsonify
-def test_multiply():
-    with app.test_client() as client:
-        response = client.get('/multiply/2/3')
-        assert response.get_data(as_text=True) == "6"
